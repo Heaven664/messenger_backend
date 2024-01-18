d6a77c8d5302657528f525ce624873fa3e462927
Omar Hamid <omarhamid664@gmail.com>
2024-01-17 22:42:15 -0700

    Add input validation for user registration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InitialUserData, User } from './interfaces/user.interface';
 import createUser from './helpers/user.factory';
+import * as emailValidator from 'email-validator';
 
 @Injectable()
 export class UsersService {
@@ -8,6 +9,30 @@ export class UsersService {
   private readonly users = [];
 
   async create(userData: InitialUserData): Promise<User> {
+    // Check if required fields are missing
+    if (!userData.email || !userData.password || !userData.name) {
+      throw new BadRequestException('Missing required fields');
+    }
+
+    // Check if user already exists
+    const userExists = this.users.find((user) => user.email === userData.email);
+    if (userExists) {
+      throw new BadRequestException('User already exists');
+    }
+
+    // Validate email
+    if (!emailValidator.validate(userData.email)) {
+      throw new BadRequestException('Invalid email');
+    }
+
+    // Validate password length
+    const MIN_PASSWORD_LENGTH = 6;
+    if (userData.password.length < MIN_PASSWORD_LENGTH) {
+      throw new BadRequestException(
+        'Password must be at least 6 characters long',
+      );
+    }
+
     // Create new user object with user factory
     const user = await createUser(userData);
     // Temporary push user to in-memory array
