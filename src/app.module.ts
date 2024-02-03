240d8ad213c494fe6bc714afa5b1bc733e3a3edd
Omar Hamid <omarhamid664@gmail.com>
2024-02-03 15:10:42 -0700

    Connect to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.237ozrg.mongodb.net/main/?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
