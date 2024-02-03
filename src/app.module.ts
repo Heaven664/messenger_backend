68dda9c45804d6918414acc27e23684851792f1d
Omar Hamid <omarhamid664@gmail.com>
2024-02-03 15:16:57 -0700

    Move connection string to separate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,14 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { MongoConnectionString } from 'config/mongo.config';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.237ozrg.mongodb.net/main/?retryWrites=true&w=majority`,
-    ),
+    MongooseModule.forRoot(MongoConnectionString),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { corsOptions } from 'cors.config';
+import { corsOptions } from 'config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
