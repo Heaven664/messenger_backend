d0d8bee0fff131bfaa07c62b5341339dad16cfd2
Omar Hamid <omarhamid664@gmail.com>
2024-02-04 12:39:40 -0700

    Add description for resolveDatabaseError function

--- src/users/helpers/customDatabaseErrorHandler.ts
+++ src/users/helpers/customDatabaseErrorHandler.ts
@@ -1,6 +1,11 @@
 import { BadRequestException } from '@nestjs/common';
 import { InputErrorMessages } from 'src/common/enums/errorMessages.enum';
 
+/**
+ * Takes error code from database and throws appropriate error
+ * @param errorCode error code from database
+ * @throws BadRequestException
+ */
 export const resolveDatabaseError = (errorCode: number) => {
   if (errorCode === 11000) {
     throw new BadRequestException(InputErrorMessages.USER_ALREADY_EXISTS);
