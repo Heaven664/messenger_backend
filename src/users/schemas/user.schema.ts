128e680d20b34a2daa616215c2aaa30009f159cc
Omar Hamid <omarhamid664@gmail.com>
2024-02-04 11:47:28 -0700

    Register Mongoose scheema for User

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop()
+  imageSrc: string;
+
+  @Prop()
+  residency: string;
+
+  @Prop()
+  lastSeenPermission: boolean;
+
+  @Prop()
+  lastSeenTime: number;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
