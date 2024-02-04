7aba23982a884e3fcd19e1382151e6aab511bfa0
Omar Hamid <omarhamid664@gmail.com>
2024-02-04 12:18:25 -0700

    Save registered user into db

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,32 +1,35 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import {
-  InitialUserData,
-  UserWithoutPassword,
-} from './interfaces/user.interface';
+import { UserWithoutPassword } from './interfaces/user.interface';
 import createUser from './helpers/user.factory';
 import validateInput from './helpers/inputValidation';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from 'src/shared/dto/create-user.dto';
 import { InputErrorMessages } from 'src/common/enums/errorMessages.enum';
 
 @Injectable()
 export class UsersService {
   // Temporary in-memory array of users
-  private readonly users = [];
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(userData: InitialUserData): Promise<UserWithoutPassword> {
+  async create(userData: CreateUserDto): Promise<UserWithoutPassword> {
     // Validate user input
     validateInput(userData);
 
-    // Check if user with the same email already exists
-    const userExists = this.users.find((user) => user.email === userData.email);
-    if (userExists) {
-      throw new BadRequestException(InputErrorMessages.UserAlreadyExists);
-    }
-
     // Create new user object with user factory
     const user = await createUser(userData);
-    // Temporary push user to in-memory array
-    this.users.push(user);
-    console.log('registered users', this.users);
+
+    try {
+      // Save user to database
+      const newUser = new this.userModel(user);
+      await newUser.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(InputErrorMessages.UserAlreadyExists);
+      }
+    }
+
     // Return newly created user without password property
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...result } = user;
