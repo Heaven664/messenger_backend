c9070a9316dc5c791b8198e72d29b5deff5ccad8
Omar Hamid <omarhamid664@gmail.com>
2024-02-29 19:11:42 -0700

    Use validation pipe and class validators to validate update-user dto

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsOptions } from 'config/cors.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable cors with the options defined in the cors.config.ts file
   app.enableCors(corsOptions);
+  // Enable global validation and transformation of received dto data
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3001);
 }
 bootstrap();
--- src/users/dto/user-dto.ts
+++ src/users/dto/user-dto.ts
@@ -1,11 +1,24 @@
+import { IsBoolean, IsEmail, IsOptional, IsString } from 'class-validator';
+
 export class UpdateUserInfoDto {
+  @IsString()
   id: string;
+
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
-  residency: string;
+
+  @IsOptional()
+  @IsString()
+  residency: string | null;
 }
 
 export class UpdateLastSeenDto {
+  @IsString()
   id: string;
+
+  @IsBoolean()
   lastSeenPermission: boolean;
 }
