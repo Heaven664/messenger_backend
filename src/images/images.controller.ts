1cc06705a8ea5514e27744ba41bc4d2d23e4e6e5
Omar Hamid <omarhamid664@gmail.com>
2024-03-14 23:22:41 -0600

    Add remove image file route

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Res } from '@nestjs/common';
 import { ImagesService } from './images.service';
 
 @Controller('images')
@@ -8,5 +8,10 @@ export class ImagesController {
   async getImage(@Param('imageUrl') imageUrl: string, @Res() res) {
     const image = await this.imagesService.getImage(imageUrl);
     return res.sendFile(image);
+  }
+
+  @Delete(':imageUrl')
+  async deleteImage(@Param('imageUrl') imageUrl: string) {
+    return this.imagesService.deleteImage(imageUrl);
   }
 }
