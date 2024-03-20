195fb02e734b6b27c85a3b72a870b21be4555898
Omar Hamid <omarhamid664@gmail.com>
2024-03-19 20:59:52 -0600

    Add private message functionality

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,20 +1,39 @@
 import {
+  ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { corsOptions } from 'config/cors.config';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
+import { AddMessageDto } from './dto/message-dto';
 
 @WebSocketGateway({ cors: corsOptions })
 export class MessagesGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer()
+  server: Server;
+
   handleConnection(client: Socket) {
     console.log('client connected', client.id);
   }
 
   handleDisconnect(client: Socket) {
     console.log('client disconnected', client.id);
+  }
+
+  @SubscribeMessage('join')
+  onJoin(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
+    // Join the room with the client email address
+    client.join(data);
+  }
+
+  @SubscribeMessage('private message')
+  onPrivateMessage(@MessageBody() message: AddMessageDto) {
+    this.server.to(message.receiverEmail).emit('private message', message);
   }
 }
