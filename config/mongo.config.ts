73aaa8af47fae4a5281ca457f3b0c22fa9ba8114
Omar Hamid <omarhamid664@gmail.com>
2024-03-24 14:09:23 -0600

    Remove console logs

--- config/mongo.config.ts
+++
@@ -1,3 +0,0 @@
-import 'dotenv/config';
-
-export const MongoConnectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.237ozrg.mongodb.net/main?retryWrites=true&w=majority`;
