0a6b135a1c3db92f8b8f2f87b65f89f05e8726a2
Omar Hamid <omarhamid664@gmail.com>
2024-03-24 14:07:34 -0600

    Config mongo connection string from proces.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,26 +5,19 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { MongoConnectionString } from 'config/mongo.config';
 import { ContactsModule } from './contacts/contacts.module';
 import { MessagesModule } from './messages/messages.module';
 import { ChatsModule } from './chats/chats.module';
 import { ImagesModule } from './images/images.module';
 
-// Configure the environment file based on the NODE_ENV environment variable
-const ENV = process.env.NODE_ENV;
-const ENV_FILE = ENV ? `.env.${ENV}` : '.env';
-console.log(`Using environment file: ${ENV_FILE}`);
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ENV_FILE,
     }),
     AuthModule,
     UsersModule,
-    MongooseModule.forRoot(MongoConnectionString),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     ContactsModule,
     MessagesModule,
     ChatsModule,
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+  console.log('process.env.ORIGIN', process.env.ORIGIN);
+  console.log('process.env.MONGO_URI', process.env.MONGO_URI);
+  console.log('process.env.JWT_SECRET_KEY', process.env.JWT_SECRET_KEY);
+  console.log('process.env.JWT_REFRESH_TOKEN', process.env.JWT_REFRESH_TOKEN);
+
   // Configure the cors options
   const corsOptions: CorsOptions = {
     origin: process.env.ORIGIN,
